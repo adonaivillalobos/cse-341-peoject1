1d4bf0b531c692249546f870ba967dbab59c12d6
adonaivillalobos <vil21022@byui.edu>
2025-03-10 18:19:06 +0100

    insert, uodate and delete

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database.js');
 const app = express();
 
 const port = process.env.PORT || 3003;
 
 app.use('/', require('./routes'));
+app.use(bodyParser.json());
 
 
 
