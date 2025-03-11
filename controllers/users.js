7ba725536a293ce2e60028e02a0a37093c475ac8
adonaivillalobos <vil21022@byui.edu>
2025-03-11 22:59:41 +0100

    get single. change in code

--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,32 @@
 const mongodb = require('../data/database.js');
-const { ObjectId } = require('mongodb').ObjectId;
+const { ObjectId } = require('mongodb');  // Corrected import
 
 const getALL = async (req, res) => {
-const result = await mongodb.getDatabase().db().collection('users').find();
+    const result = await mongodb.getDatabase().db().collection('users').find();
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users);
-    })
-}
+    }).catch((error) => {
+        res.status(500).json({ message: "Failed to retrieve users", error });
+    });
+};
 
 const getSingle = async (req, res) => {
-const userID = new ObjectId(req.params.id);
-const result = await mongodb.getDatabase().db().collection('users').find({_id: userId});
-    result.toArray().then((users) => {
+    try {
+        const userID = new ObjectId(req.params.id);  // Correct usage of ObjectId
+        const user = await mongodb.getDatabase().db().collection('users').findOne({ _id: userID });  // Use findOne to get a single document
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users)[0];
-    })
+        res.status(200).json(user);  // Returning the user object directly
+    } catch (error) {
+        res.status(500).json({ message: "Error retrieving user", error });
+    }
 };
+
 
 
 const createUser = async (req, res) => { 
