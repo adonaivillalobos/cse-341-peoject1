16c021c06504291698d71f2d3269cf40c40d04bf
adonaivillalobos <vil21022@byui.edu>
2025-03-12 13:24:20 +0100

    new update on users and routes

--- controllers/users.js
+++ controllers/users.js
@@ -63,7 +63,7 @@ const createUser = async (req, res) => {
 
  const deleteUser = async (req, res) => {
     const userId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('users').remove({_id: userId}, true);
+    const response = await mongodb.getDatabase().db().collection('users').deleteOne({_id: userId});
     if (response.deletedCount > 0) {
         res.status(204).send();
     } else {
