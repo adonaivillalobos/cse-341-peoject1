7bd0974a747f1cc51f009a219fd06cb6fb2c0902
adonaivillalobos <vil21022@byui.edu>
2025-03-13 22:07:20 +0100

    delete http

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: 'Users API',
     },
     host: 'localhost:3003',
-    schemes: ['http', 'https']
+    schemes: ['https']
 };
 
 const outputFile = './swagger.json';
