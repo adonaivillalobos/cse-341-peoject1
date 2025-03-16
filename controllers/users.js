2de726f796f31d23fb2a7e2de3b76245f7a60077
adonaivillalobos <vil21022@byui.edu>
2025-03-16 05:14:37 +0100

    syntax

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const mongodb = require('../data/database');
-const ObjectId= require('mongodb.').ObjectId;
+const ObjectId= require('mongodb').ObjectId;
 
 const getALL = async (req, res) => {
     //#swagger.tags = ['Users']
